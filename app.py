e309b95ad23cbb637bf2db53b7fbc56b5c5e0433
lolz <dkiran100@gmail.com>
2022-09-20 20:56:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from transformers import pipeline
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    print("Text generator")
+    return("Hello")
+
+@app.route('/gen',methods=["POST"])
+def generate():
+    t = ""
+    txt = ""
+
+    req_Json = request.json
+    txt = req_Json['text']
+
+    gen = pipeline(task = 'text-generation', model = 'gpt2')
+    t = gen(txt, max_length = 1000, num_return_sequences = 1)
+
+    return(t[0])  
+
+@app.route('/genhtml',methods=["GET", "POST"])
+def generatehtml():
+    t = ""
+    txthtml = ""
+    if request.method == "POST":
+        txthtml = request.form.get('txt')
+        gen = pipeline(task = 'text-generation', model = 'gpt2')
+        t = gen(txthtml, max_length = 1000, num_return_sequences = 1)
+
+    return render_template('index.html', generate = t)
